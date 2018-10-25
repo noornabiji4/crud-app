9379d6ab1075a5ec3effb6b22386d743b58ab929
Noor <noornabiji4@gmail.com>
2018-10-25 23:12:05 +0530

    user schema

---
+++ models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose')
+const userSchema = new mongoose.Schema({
+    username: {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String
+    },
+    password:{
+        type : String
+    },
+    date : {
+        type : Date,
+        default : Date.now()
+    },
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User  = mongoose.model('User' , userSchema);
+
+module.exports = User ; 
