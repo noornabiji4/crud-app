f65c0da80328ae8f4b06d58e8cabd85a077f4f7f
Noor <noornabiji4@gmail.com>
2018-10-25 23:06:16 +0530

    main schema file

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost/',(err)=>{ //enter ur api name
+mongoose.connect('mongodb://localhost/profile',(err)=>{ //enter ur api name
     if(err){
         console.log(err);
     }else {
-        console.log("Database is connceted");
+        console.log("Database is connceted ");
     } 
 });
+
+module.exports.Profile = require('./profile');
+module.exports.User=require('./users');
 
 mongoose.Promise = Promise;
